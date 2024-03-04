e6730d6508e22a270650b2aafbc92c1287c39175
isumalatov <isumalatov@gmail.com>
2024-03-04 15:15:45 +0100

    tcalendario finished

--- tcalendario.cpp
+++ tcalendario.cpp
@@ -102,5 +102,199 @@ TCalendario TCalendario::operator+(int n) {
     return c;
 }
 
+TCalendario TCalendario::operator-(int n) {
+    if (n < 0) {
+        return *this;
+    }
+    TCalendario c(*this);
+    for (int i = 0; i < n; i++) {
+        c.dia--;
+        if (c.dia < 1) {
+            c.mes--;
+            if (c.mes < 1) {
+                c.mes = 12;
+                c.anyo--;
+            }
+            c.dia = daysInMonth(c.mes, c.anyo);
+        }
+    }
 
+    if(c.anyo < 1900) {
+        c.dia = 1;
+        c.mes = 1;
+        c.anyo = 1900;
+        if(c.mensaje != NULL) {
+            delete[] c.mensaje;
+            c.mensaje = NULL;
+        }
+    }
+    
+    return c;
+}
 
+TCalendario TCalendario::operator++(int) {
+    TCalendario c(*this);
+    dia++;
+    if (dia > daysInMonth(mes, anyo)) {
+        dia = 1;
+        mes++;
+        if (mes > 12) {
+            mes = 1;
+            anyo++;
+        }
+    }
+    return c;
+}
+
+TCalendario & TCalendario::operator++() {
+    dia++;
+    if (dia > daysInMonth(mes, anyo)) {
+        dia = 1;
+        mes++;
+        if (mes > 12) {
+            mes = 1;
+            anyo++;
+        }
+    }
+    return *this;
+}
+
+TCalendario TCalendario::operator--(int) {
+    TCalendario c(*this);
+    dia--;
+    if (dia < 1) {
+        mes--;
+        if (mes < 1) {
+            mes = 12;
+            anyo--;
+        }
+        dia = daysInMonth(mes, anyo);
+    }
+    if(anyo < 1900) {
+        dia = 1;
+        mes = 1;
+        anyo = 1900;
+        if(mensaje != NULL) {
+            delete[] mensaje;
+            mensaje = NULL;
+        }
+    }
+    return c;
+}
+
+TCalendario & TCalendario::operator--() {
+    dia--;
+    if (dia < 1) {
+        mes--;
+        if (mes < 1) {
+            mes = 12;
+            anyo--;
+        }
+        dia = daysInMonth(mes, anyo);
+    }
+    if(anyo < 1900) {
+        dia = 1;
+        mes = 1;
+        anyo = 1900;
+        if(mensaje != NULL) {
+            delete[] mensaje;
+            mensaje = NULL;
+        }
+    }
+    return *this;
+}
+
+bool TCalendario::ModFecha(int d, int m, int a) {
+    if (a < 1900 || m < 1 || m > 12 || d < 1 || d > daysInMonth(m, a)) {
+        return false;
+    }
+    dia = d;
+    mes = m;
+    anyo = a;
+    return true;
+}
+
+bool TCalendario::ModMensaje(char * mens) {
+    delete[] mensaje;
+    if (mens != NULL) {
+        mensaje = new char[strlen(mens) + 1];
+        strcpy(mensaje, mens);
+    } else {
+        mensaje = NULL;
+    }
+    return true;
+}
+
+bool TCalendario::operator==(TCalendario &c) {
+    bool iguales;
+
+    iguales = dia == c.dia && mes == c.mes && anyo == c.anyo;
+
+    if (iguales) {
+        if (mensaje == NULL && c.mensaje == NULL) {
+            iguales = true;
+        } else if (mensaje == NULL || c.mensaje == NULL) {
+            iguales = false;
+        } else {
+            iguales = strcmp(mensaje, c.mensaje) == 0;
+        }
+    }
+
+    return iguales;
+}
+
+bool TCalendario::operator!=(TCalendario &c) {
+    return !(*this == c);
+}
+
+bool TCalendario::operator>(TCalendario &c) {
+    if (anyo > c.anyo) {
+        return true;
+    } else if (anyo < c.anyo) {
+        return false;
+    } else if (mes > c.mes) {
+        return true;
+    } else if (mes < c.mes) {
+        return false;
+    } else if (dia > c.dia) {
+        return true;
+    } else if (dia < c.dia) {
+        return false;
+    } else if (mensaje == NULL && c.mensaje == NULL) {
+        return false;
+    } else if (mensaje == NULL)
+        return false;
+    else if (c.mensaje == NULL)
+        return true;
+    else
+        return strcmp(mensaje, c.mensaje) > 0;
+}
+
+bool TCalendario::operator<(TCalendario &c) {
+    return !(*this > c || *this == c);
+}
+
+bool TCalendario::EsVacio() {
+    return dia == 1 && mes == 1 && anyo == 1900 && mensaje == NULL;
+}
+
+int TCalendario::Dia() {
+    return dia;
+}
+
+int TCalendario::Mes() {
+    return mes;
+}
+
+int TCalendario::Anyo() {
+    return anyo;
+}
+
+char * TCalendario::Mensaje() {
+    return mensaje;
+}
+
+ostream & operator<<(ostream &os, TCalendario &c) {
+    os << c.Dia() << "/" << c.Mes() << "/" << c.Anyo() << " \"" << c.Mensaje() << "\"";
+    return os;
+}
